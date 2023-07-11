2f568caaab4e838dc79d64b468c5dd5aad05e140
AzhdanYulmi <azhdan@protonmail.com>
2023-07-11 19:37:49 +0300

    FIX: Added the Repository annotation

--- src/main/java/Repository/UserRepository.java
+++ src/main/java/Repository/UserRepository.java
@@ -2,7 +2,9 @@ package Repository;
 
 import Entity.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<AppUser, Integer> {
 
 }
