efbcaf8123b062db9d6e8a627a9cb19336b52b65
AzhdanYulmi <azhdan@protonmail.com>
2023-07-11 13:41:12 +0300

    ADD: User DTO

---
+++ src/main/java/DTO/AppUserDTO.java
@@ -0,0 +1,73 @@
+package DTO;
+
+import java.time.LocalDate;
+
+public class AppUserDTO {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String phoneNumber;
+    private String emailAddress;
+
+    public AppUserDTO() {
+    }
+
+    public AppUserDTO(int id, String firstName, String lastName, LocalDate dateOfBirth, String phoneNumber, String emailAddress) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+}
+
